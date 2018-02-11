ea75b772feed3d07e3d53672dc8a173401d0fe34
A.M. Masudul Haque <saadixl@outlook.com>
2018-02-11 18:42:02 +0600

    Changing title

---
+++ sticky-warning/main.js
@@ -0,0 +1,1 @@
+$("body").append( '<div style="z-index:9999999;background:transparent;position:fixed;width:100%;top:0;left:0;color:#fff;margin:0;text-align:center;font-size:12px;"><span style="background:red; padding: 0 5px;">YOUR MESSAGE SHOULD BE HERE!</span></div>');
